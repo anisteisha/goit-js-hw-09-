d6739226aff808e66477ee90d2ede88f9029ce3b
anisteisha <lereenaarakurt@gmail.com>
2024-06-13 19:43:05 +0100

    GH9F

---
+++ src/js/2-form.js
@@ -0,0 +1,37 @@
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const formData = {
+    email: "",
+    message: ""
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const savedData = localStorage.getItem('feedback-form-state');
+    if (savedData) {
+        const { email, message } = JSON.parse(savedData);
+        formData.email = email;
+        formData.message = message;
+        emailInput.value = email;
+        messageInput.value = message;
+    }
+});
+
+form.addEventListener('input', (event) => {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (formData.email === "" || formData.message === "") {
+        alert("Fill please all fields");
+    } else {
+        console.log(formData);
+        localStorage.removeItem('feedback-form-state');
+        form.reset();
+        formData.email = "";
+        formData.message = "";
+    }
+});
